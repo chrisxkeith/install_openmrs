3bda8d91fb3d4c04632bc68954fb570a03a1badd
Chris Keith <chris.keith@gmail.com>
2026-02-20 16:22:33 -0800

    clean and clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
-# DANGER ! This script deletes your current local repo and local openmrs app(s)
+# DANGER !
+# This script deletes your current local repo
+# and local openmrs app, e.g., ~/openmrs
 # in an attempt to start with a clean slate.
+
 # Assumes:
-# - You have forked ...
-# - You have installed maven
-# - You have installed and plan to use Docker
+# - You have forked https://github.com/openmrs/openmrs-core.
+# - You have installed maven.
+# - You have installed and plan to use Docker.
+# - You are running this script from the parent directory of your (to-be-created) local repo.
+# - Your github username is in a shell variable called $GITHUB_USERNAME.
+# - You are willing to wait (at least) a few minutes.
 
 set -x
+if [ -z "$GITHUB_USERNAME" ] ; then
+	echo "Please set the GITHUB_USERNAME environment variable to your github username."
+	echo "Example: export GITHUB_USERNAME=chrisxkeith"
+	exit -665
+fi
 mvn --version
 if [ $? -ne 0 ] ; then
 	echo "No maven install detected."
@@ -24,3 +35,23 @@ if [ $v -gt 27 ] ; then
   echo "Needs docker 27, not $v"
   exit -668
 fi
+if [ -d "~/openmrs" ] ; then
+	rm -rf ~/openmrs
+	if [ $? -ne 0 ] ; then
+		echo "Failed to delete ~/openmrs"
+		exit -669
+	fi	
+fi
+if [ -d "openmrs-core" ] ; then
+	rm -rf openmrs-core
+	if [ $? -ne 0 ] ; then
+		echo "Failed to delete local repo."
+		exit -670
+	fi	
+fi
+git clone https://github.com/${GITHUB_USERNAME}/openmrs-core.git
+if [ $? -ne 0 ] ; then
+	echo "Failed to clone repo."
+	exit -671
+fi
+cd openmrs-core
